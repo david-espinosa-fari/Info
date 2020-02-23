cf1f367f6d0f6c9865201708455cf78f525bf816
David-Espinosa-Fari <david.espinosa@students.salle.url.edu>
2020-02-23 21:21:34 +0100

    Created userError for exceptions

---
+++ App/src/Users/Domain/UserException/UserError.js
@@ -0,0 +1,23 @@
+class UserError
+{
+    message;
+    code;
+    constructor(message, code){
+        this.message = message;
+        this.code = code;
+    }
+    get message(){
+        return this.message;
+    };
+    get code(){
+        return this.code;
+    };
+    set message(message){
+        this.message = message;
+    };
+    set code(code){
+        this.code = code;
+    };
+}
+
+module.exports = UserError;
