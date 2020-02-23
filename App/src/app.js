6e1196eb0989a697aaa62e5829e51067230f7b2e
David-Espinosa-Fari <david.espinosa@students.salle.url.edu>
2020-02-23 21:29:35 +0100

    Added urlencoded for post request

--- App/src/app.js
+++ App/src/app.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/', index);
-
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log('App listening on port ' + port + ' !'));
 
 
 
