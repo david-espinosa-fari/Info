6f43dd259773ee3dbbefb6a45720e158a773a4ed
David-Espinosa-Fari <david.espinosa@students.salle.url.edu>
2020-02-23 23:00:34 +0100

    I have removed the instanceof verification because if there is no user.password it is not the user object

--- App/src/Users/Controller/postLoginController.js
+++ App/src/Users/Controller/postLoginController.js
@@ -3,12 +3,11 @@ const jwt = require('jwt-simple');
 const config = require('../../config/config');
 const findUserByUserName = require('../Aplication/findUserByUsername');
 const userRepo = require('../Infraestructure/fakeDbUsersRepository');
-const userError = require('../Domain/UserException/UserError');
 const callback = function(req, res){
     const username = req.body.username;
     const passwordHash = new hash.SHA256(req.body.password).hex(req.body.password);
     const user = findUserByUserName(userRepo).invoke(username);
-    if (passwordHash === user.password && user instanceof !userError){
+    if (passwordHash === user.password){
         const signedKey = config.TOKEN_SECRET;
         const tokenPayload = {
             'username': username,
