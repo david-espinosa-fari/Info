e64cdf9edbc79894a50907e755f1ebb6ff525441
David-Espinosa-Fari <david.espinosa@students.salle.url.edu>
2020-02-23 20:57:54 +0100

    Returns a list of users, because I understand that the name can be repeated

---
+++ App/src/Users/Controller/getUsersByNameController.js
@@ -0,0 +1,24 @@
+const filterUsersByName = require('../Aplication/filterUsersByName');
+const mockyRepo = require('../Infraestructure/mockyUsersRepository');
+const jwt = require('jwt-simple');
+const config = require('../../config/config');
+const callback = function(req, res){
+    try {
+        const payload = jwt.decode(req.headers.authorization, config.TOKEN_SECRET);
+        console.log(payload);
+        if (payload.role !== 'admin' && payload.role !== 'user'){
+            throw new Error('User not admin');
+        }
+        filterUsersByName(mockyRepo).invoke(req.query.name)
+            .then(function(users){
+                //I return an array because I understand that names can be repeated
+                res.status(200).send(JSON.stringify(users));
+            })
+            .catch(function(ClientsError){
+                res.status(ClientsError.code).send(ClientsError.message);
+            });
+    } catch (e){
+        return res.status(403).send('User not authorized');
+    }
+};
+module.exports = callback;
